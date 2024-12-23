23c7dc9adb7e8a1ac8ba4b78500665d9ecc985d3
Nikita Manjhi <nikitamanjhi540@gmail.com>
2024-12-23 22:26:56 +0530

    fixed issue

--- app/(dashboard)/(routes)/search/[jobId]/resources/page.tsx
+++ app/(dashboard)/(routes)/search/[jobId]/resources/page.tsx
@@ -1,5 +1,5 @@
 import { db } from "@/lib/db";
-import { auth } from "@clerk/nextjs/server";
+// import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { JobResourcesContent } from "./_components/job_resources";
 
